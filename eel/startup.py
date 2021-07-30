0fe3b162984067d960d00ac488a490c70b3600f7
clark-fischer <clark.fischer04@g.riverhead.net>
2021-07-30 13:36:30 -0400

    basic pagination
    :smile:

--- eel/startup.py
+++ eel/startup.py
@@ -10,19 +10,33 @@ index_page = init + r'\html\index.html'
 
 con = sqlite3.connect('app.db')
 cur = con.cursor()
+
 # cur.execute('DROP TABLE tasks')
 # cur.execute("CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, task VARCHAR(255), creator VARCHAR(255), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, due_at DATETIME, description VARCHAR(255), status_id INT, users_assigned1 VARCHAR(255), users_assigned2 VARCHAR(255), users_assigned3 VARCHAR(255));")
-# cur.execute("INSERT INTO tasks (task, creator, due_at, description, status_id, users_assigned1) values ('Create App', 'Clark', '2021-07-31 12:00:00', 'Creating this very app!', '1', 'Clark'); ")
+# cur.execute("INSERT INTO tasks (task, creator, due_at, description, status_id, users_assigned1) values ('Create App3', 'James', '2021-07-31 12:00:00', 'Creating this very app!', '1', 'Clark'); ")
+con.commit()
+
+@eel.expose
+def get_task_length():
+	n = cur.execute('SELECT MAX(id) from tasks')
+	for i in n:
+		return i[0]
 
 @eel.expose 
-def get_rows(command='SELECT * from tasks;'):
-	for row in cur.execute(command):
-		return(row)
+def get_rows(num, command='SELECT task, creator, due_at, description, status_id, users_assigned1 from tasks;', where=''):
+	lines = []
+	for row in cur.execute(command + where):
+		lines.append(row)
+	return lines[num]
 
-get_rows()
-con.commit()
+
+@eel.expose 
+def hello():
+	print('hello')
+
+get_task_length()
+
+eel.init(init)
+eel.start(index_page, size=(720, 720))
+
 con.close()
-
-
-# eel.init(init)
-# eel.start(index_page, size=(720, 720))
