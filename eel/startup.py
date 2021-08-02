ff17a5cc18e2eda55e53af2e393243d350a3e49c
clark-fischer <clark.fischer04@g.riverhead.net>
2021-08-02 16:37:47 -0400

    saving for the day
    need to find a way to save options

--- eel/startup.py
+++ eel/startup.py
@@ -14,7 +14,7 @@ cur = con.cursor()
 # cur.execute('DROP TABLE tasks')
 # cur.execute("CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, task VARCHAR(255), creator VARCHAR(255), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, due_at DATETIME, description VARCHAR(255), status_id INT, users_assigned1 VARCHAR(255), users_assigned2 VARCHAR(255), users_assigned3 VARCHAR(255));")
 # cur.execute("INSERT INTO tasks (task, creator, due_at, description, status_id, users_assigned1) values ('Create App3', 'James', '2021-07-31 12:00:00', 'Creating this very app!', '1', 'Clark'); ")
-con.commit()
+
 
 @eel.expose
 def get_task_length():
@@ -29,14 +29,20 @@ def get_rows(num, command='SELECT task, creator, due_at, description, status_id,
 		lines.append(row)
 	return lines[num]
 
-
 @eel.expose 
 def hello():
 	print('hello')
 
-get_task_length()
+@eel.expose
+def println(stuff):
+	print(stuff)
+
+@eel.expose	
+def update_db(num, command='UPDATE tasks SET status_id = 2 WHERE id = '):
+	cur.execute(command + num + ';')
 
 eel.init(init)
 eel.start(index_page, size=(720, 720))
 
+con.commit()
 con.close()
