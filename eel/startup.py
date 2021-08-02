8f68dca8563a9fcce81e241459249f8f01845658
clark-fischer <clark.fischer04@g.riverhead.net>
2021-08-02 16:46:17 -0400

    buttons work
    tiny little change and now buttons work. Who'd have thought? It's highly inefficient but who cares!

--- eel/startup.py
+++ eel/startup.py
@@ -14,8 +14,8 @@ cur = con.cursor()
 # cur.execute('DROP TABLE tasks')
 # cur.execute("CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, task VARCHAR(255), creator VARCHAR(255), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, due_at DATETIME, description VARCHAR(255), status_id INT, users_assigned1 VARCHAR(255), users_assigned2 VARCHAR(255), users_assigned3 VARCHAR(255));")
 # cur.execute("INSERT INTO tasks (task, creator, due_at, description, status_id, users_assigned1) values ('Create App3', 'James', '2021-07-31 12:00:00', 'Creating this very app!', '1', 'Clark'); ")
-
-
+for i in cur.execute("select * from tasks"):
+	print(i)
 @eel.expose
 def get_task_length():
 	n = cur.execute('SELECT MAX(id) from tasks')
@@ -38,8 +38,15 @@ def println(stuff):
 	print(stuff)
 
 @eel.expose	
-def update_db(num, command='UPDATE tasks SET status_id = 2 WHERE id = '):
-	cur.execute(command + num + ';')
+def set_complete(num, command='UPDATE tasks SET status_id = 2 WHERE id = '):
+	print(command + str(num))
+	cur.execute(command + str(num) + ';')
+	con.commit()
+@eel.expose	
+def set_incomplete(num, command='UPDATE tasks SET status_id = 1 WHERE id = '):
+	print(command + str(num))
+	cur.execute(command + str(num) + ';')
+	con.commit()
 
 eel.init(init)
 eel.start(index_page, size=(720, 720))
