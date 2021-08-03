b6db58de3656a0a51464439add4ff8fa1f8dc656
clark-fischer <clark.fischer04@g.riverhead.net>
2021-08-03 12:58:31 -0400

    Removed search and made form work
    hee hge

--- eel/startup.py
+++ eel/startup.py
@@ -14,11 +14,20 @@ cur = con.cursor()
 # cur.execute('DROP TABLE tasks')
 # cur.execute("CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, task VARCHAR(255), creator VARCHAR(255), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, due_at DATETIME, description VARCHAR(255), status_id INT, users_assigned1 VARCHAR(255), users_assigned2 VARCHAR(255), users_assigned3 VARCHAR(255));")
 # cur.execute("INSERT INTO tasks (task, creator, due_at, description, status_id, users_assigned1) values ('Create App3', 'James', '2021-07-31 12:00:00', 'Creating this very app!', '1', 'Clark'); ")
+
+
 for i in cur.execute("select * from tasks"):
 	print(i)
+
 @eel.expose
-def get_task_length(num):
-	n = cur.execute('SELECT MAX(id) FROM tasks WHERE status_id = ' + str(num))
+def get_task_length_complete():
+	n = cur.execute('SELECT MAX(id) FROM tasks WHERE status_id = 2')
+	for i in n:
+		return i[0]
+
+@eel.expose
+def get_task_length_incomplete():
+	n = cur.execute('SELECT MAX(id) FROM tasks WHERE status_id = 1')
 	for i in n:
 		return i[0]
 
@@ -55,11 +64,20 @@ def set_complete(num, command='UPDATE tasks SET status_id = 2 WHERE id = '):
 	print(command + str(num))
 	cur.execute(command + str(num) + ';')
 	con.commit()
+
 @eel.expose	
 def set_incomplete(num, command='UPDATE tasks SET status_id = 1 WHERE id = '):
 	print(command + str(num))
 	cur.execute(command + str(num) + ';')
 	con.commit()
+
+@eel.expose	
+def form_insert(task, creator, due_at, description, users_assigned1, users_assigned2, users_assigned3):
+	command = f"INSERT INTO tasks (task, creator, due_at, description, status_id, users_assigned1, users_assigned2, users_assigned3) values ('{task}', '{creator}', '{due_at}', '{description}', '1', '{users_assigned1}',  '{users_assigned2}', '{users_assigned3}'); "
+	
+	cur.execute(command)
+	con.commit()
+
 
 eel.init(init)
 eel.start(index_page, size=(720, 720))
